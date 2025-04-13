8fc0652f378f441b9597a397d64dbdc6558df5fb
belkakomelka <kamilya0902@yandex.ru>
2025-04-13 17:27:03 +0300

    getAllProducts added method to controller

--- src/main/java/com/example/demo/controller/ProductsController.java
+++ src/main/java/com/example/demo/controller/ProductsController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.product.ProductAddRq;
 import com.example.demo.service.products.AddProductService;
+import com.example.demo.service.products.GetAllProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,18 @@ public class ProductsController {
     @Autowired
     AddProductService addProductService;
 
+    @Autowired
+    GetAllProductService getAllProductService;
+
     @PostMapping(path = "/add-product", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<String> addProduct(@RequestPart("file") MultipartFile image,
                                              @RequestPart("data") ProductAddRq productAddRq) {
         return addProductService.addProduct(productAddRq, image);
+    }
+
+    @GetMapping("all-products")
+    public ResponseEntity<String> getAllProducts(){
+        return getAllProductService.getAllProducts();
     }
 
 }
